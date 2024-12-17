6c413a5e0637487934da0e0f372f3f290da52238
anirudha <anirudhadonge@hotmail.com>
2024-12-17 07:24:13 +0530

    Allure Reporting Added. Script added in the package.json file.

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,8 +22,10 @@ export default defineConfig({
   
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
+    ['line'],
     ["html", { open: "never", outputFolder: "./test-results" }],
     ["junit", { outputFile: "./test-results/results/results.xml" }],
+    ["allure-playwright"]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
